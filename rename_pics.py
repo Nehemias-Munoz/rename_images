1cbf49eec27e9e2a7e78ae6f7fab54abcd53a13b
Nehemias-Munoz <nehemiasmunozm@gmail.com>
2022-08-09 15:35:04 -0400

    Commit inicial

---
+++ rename_pics.py
@@ -0,0 +1,14 @@
+
+# import os.path, time, os
+
+# folder_path = '/Users/nehemiasmunoz/Library/CloudStorage/OneDrive-UNIVERSIDADANDRESBELLO/Pictures/Screenshots'
+# os.chdir(folder_path)
+# if __name__ == '__main__':
+#     for file_name in os.listdir(folder_path):
+#         name, extension = os.path.splitext(folder_path + file_name)
+#         # print(f'fecha creacioin {time.ctime(os.path.getctime(file_name))}')
+
+#         # if extension in ['.jpg', '.jpeg', '.png']:
+#         #     if name
+
+
